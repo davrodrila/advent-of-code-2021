1320fe36cdc139fe06760d50f74a4a5016be13ef
David <davrodrila@gmail.com>
2021-12-01 22:50:27 +0100

    slight refactor to make things a little bit nicer

--- day1/day1.go
+++ day1/day1.go
@@ -11,61 +11,8 @@ func main() {
 
 	numbers := readFileAsNumbers("input.txt")
 
-	previousValue := 0
-	increments := 0
-	decrements := 0
-
-	for _, number := range numbers {
-		if previousValue != 0 {
-			if number > previousValue {
-				increments++
-			}
-			if number < previousValue {
-				decrements++
-			}
-		}
-		previousValue = number
-	}
-
-	fmt.Printf("Day 1 first solution is: Increments: %d and decrements: %d\n", increments, decrements)
-
-	previousValue = 0
-	increments = 0
-	decrements = 0
-
-	for i, number := range numbers {
-		secondSum := getSumNumber(numbers, i, 1)
-		thirdSum := getSumNumber(numbers, i, 2)
-
-		sum := number + secondSum + thirdSum
-		if previousValue != 0 {
-			if sum > previousValue {
-				increments++
-			}
-			if sum < previousValue {
-				decrements++
-			}
-		}
-
-		previousValue = sum
-	}
-
-	fmt.Printf("Day 1 second solution is: Increments: %d and decrements: %d\n", increments, decrements)
-}
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-func getSumNumber(numbers []int, index int, offset int) int {
-	value := 0
-	if index+offset < len(numbers) {
-		value = numbers[index+offset]
-	}
-
-	return value
+	doFirstDay(numbers)
+	doSecondDay(numbers)
 }
 
 func readFileAsNumbers(path string) []int {
@@ -86,10 +33,72 @@ func readFileAsNumbers(path string) []int {
 	return numbers
 }
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func convertLineToNumber(scanner bufio.Scanner) int {
 	line := scanner.Text()
 	number, e := strconv.Atoi(line)
 	check(e)
 
 	return number
+}
+
+func doFirstDay(numbers []int) {
+
+	previousValue := 0
+	increments := 0
+	decrements := 0
+
+	for _, number := range numbers {
+		increments, decrements = checkValueChanges(previousValue, number, increments, decrements)
+		previousValue = number
+	}
+
+	fmt.Printf("Day 1 first solution is: Increments: %d and decrements: %d\n", increments, decrements)
+
+}
+
+func doSecondDay(numbers []int) {
+	previousValue := 0
+	increments := 0
+	decrements := 0
+
+	for i, number := range numbers {
+		secondSum := getSumNumber(numbers, i, 1)
+		thirdSum := getSumNumber(numbers, i, 2)
+
+		currentValue := number + secondSum + thirdSum
+
+		increments, decrements = checkValueChanges(previousValue, currentValue, increments, decrements)
+
+		previousValue = currentValue
+	}
+
+	fmt.Printf("Day 1 second solution is: Increments: %d and decrements: %d\n", increments, decrements)
+}
+
+func getSumNumber(numbers []int, index int, offset int) int {
+	value := 0
+	if index+offset < len(numbers) {
+		value = numbers[index+offset]
+	}
+
+	return value
+}
+
+func checkValueChanges(previousValue int, currentValue int, increments int, decrements int) (int, int) {
+	if previousValue != 0 {
+		if currentValue > previousValue {
+			increments++
+		}
+		if currentValue < previousValue {
+			decrements++
+		}
+	}
+
+	return increments, decrements
 }
